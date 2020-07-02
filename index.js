d0d689a770052b7d01213440a492593a8c644925
tothpeti2000 <toth.peti26@gmail.com>
2020-07-02 18:25:47 +0200

    commit

--- index.js
+++ index.js
@@ -202,8 +202,8 @@ for (let i = 0; i < n; i++) {
   let random_x = Math.floor(Math.random() * (canvas.width - 100)) + 50;
   let random_y = Math.floor(Math.random() * (canvas.height - 100)) + 50;
   let random_m = Math.floor(Math.random() * 25) + 1;
-  let random_v_x = Math.floor(Math.random() * 7) + 1;
-  let random_v_y = Math.floor(Math.random() * 7) + 1;
+  let random_v_x = Math.floor(Math.random() * 5) + 1;
+  let random_v_y = Math.floor(Math.random() * 5) + 1;
 
   balls.push(new Ball(random_x, random_y, random_v_x, random_v_y, random_m));
 
